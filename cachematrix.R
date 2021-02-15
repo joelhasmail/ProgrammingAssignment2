17e59e3ada5bf76be7abe07a4f8fdf8a670846d6
joelhasmail <joelhasmail@gmail.com>
2021-02-15 17:05:28 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
+## Take the mean function given before and change it to fit an inverse.
+## None of this is ever explained in the lessons or swirl.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function(){x}
+  setInverse <- function(inverse){inv <<- inverse}
+  getInverse <- function(){inv}
+  list(set = set, get= get, setInverse = setInverse, getInverse = getInverse)
+  
 }
 
 
 ## Write a short comment describing this function
-
+#check if getinverse exists and returns it, or calculates if it is a NULL value
 cacheSolve <- function(x, ...) {
+  inv <- x$getinverse
+  if (!is.null(inverse)){
+    return(inv)
+  }
+  mat <<- x$get()
+  inv <- solve(mat, ...)
+  x$getInverse(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
